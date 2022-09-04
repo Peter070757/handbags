8b5e1fca2f888ee7eef5a65b5336e2c6c4eb4ad0
Peter070757 <petergroeneveld@home.nl>
2022-09-04 12:12:59 +0200

    Stappenplan Buttons-1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
 
+//Handbags & Purses Stappenplan Buttons-1
+
 function App() {
-  return (
-      <h1>Handbags & Purses</h1>
-  );
+    return (
+        <>
+            <h1>Handbags & Purses</h1>
+            <nav>
+                <button type='button' onClick={() => console.log("to the collection")}>
+                    to the collection
+                </button>
+                <button type='button' onClick={() => console.log("shop all bags")}>
+                    shop all bags
+                </button>
+                <button type='button' onClick = {() => console.log("pre-orders")} disabled>
+                    pre-orders
+                </button>
+            </nav>
+        </>
+
+    );
 }
 
 export default App;
